29a2c957d574bba16ca4a9841c9aeffb7576ca8f
陈威吉 <@LAPTOP-VSHJ1OSI.mshome.net>
2019-03-27 18:57:50 +0800

    修改

--- src/main/java/org/cwjweixin/weixin/controller/MessageReceiverController.java
+++ src/main/java/org/cwjweixin/weixin/controller/MessageReceiverController.java
@@ -1,4 +1,5 @@
 package org.cwjweixin.weixin.controller;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 // 路径和类的映射关系
 // <url-pattern> 用于映射URL和Servlet的关系
 // 如果多人共享一台服务器，把kemao_2改为姓名的拼音
-@RequestMapping("/kemao_2/message/receiver")
+@RequestMapping("/weixin_2/message/receiver")
 public class MessageReceiverController {
 
 	// 注意：控制器里面必须要有处理器方法（Handler Method）才能执行操作，才不会404
